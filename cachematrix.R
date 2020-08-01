e6c2c5b443255b2abe264f1cf59020a5eda4172e
Donahub3 <Brenden.Donahue@gmail.com>
2020-07-31 19:03:59 -0700

    getting Ready to Push

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,35 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##Initalizes the Matrix object used in the cachedSolve function 
+makeCacheMatrix <- function(x = matrix()) {##sets x default to empty matrix
+  m<-NULL ##Clears value in m
+  set<-function(y) {
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x #gets value x
+  setinv<-function(solve) m<<-solve #sets inverse
+  getinv<-function() m #gets inverse
+  
+  list(set=set, get=get,setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
-
+##Solves for the inverse of a matrix using the makeCachedMatrix value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinv()
+  if(!is.null(m)) {
+    message("Getting Cached Data")
+    return(m)
+  }
+  data=matrix()
+  data<-x$get()
+  m<- solve(a=data)
+  x$setinv(m)
+  m
+  
+  
+  ## Return a matrix that is the inverse of 'x'
 }
